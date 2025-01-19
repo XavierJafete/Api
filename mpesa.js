a18bb61e375787351cb0d8c9ce66d057e4f26b64
XavierJafete <89250022+XavierJafete@users.noreply.github.com>
2025-01-19 23:44:28 +0000

    Update mpesa.js

--- mpesa.js
+++ mpesa.js
@@ -1,11 +1,5 @@
 // api/mpesa.js
-module.exports = async (req, res) => {
-  try {
-    // Sua lógica de integração com o Mpesa aqui
-    res.status(200).json({
-      message: 'Integração com Mpesa funcionando!',
-    });
-  } catch (error) {
-    res.status(500).json({ error: 'Erro ao processar a solicitação' });
-  }
+module.exports = (req, res) => {
+  res.status(200).json({ message: 'Integração com Mpesa funcionando!' });
 };
+

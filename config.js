4ee901932a6ce148dd61b9df086c039d67a76321
XavierJafete <89250022+XavierJafete@users.noreply.github.com>
2025-01-20 00:25:23 +0000

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  apiUrl: 'https://api.safaricom.co.ke',  // URL da API do Mpesa
+  username: 'your_username',
+  password: 'your_password',
+  shortcode: 'your_shortcode',
+  lipaNaMpesaOnlineShortcode: 'your_shortcode',
+};
